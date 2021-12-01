eb81c41ed9b3f8689d694d99c400a8b72e0bd939
anazareth <anazareth@ryerson.ca>
2021-12-01 16:35:07 -0500

    day 1 part 1 initial

--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,23 @@
-import urllib.request
+# Problem: https://adventofcode.com/2021/day/1
+# Date: December 1st, 2021
+# Author: Alex Nazareth
 
-DATA_URL = r'https://adventofcode.com/2021/day/1/input'
+import numpy as np
+
+INFILE = r'day1/day1_input'
 
 
 def main():
-    with urllib.request.urlopen(DATA_URL) as response:
-        html = response.read()
-    print(type(html))
+    in_data = np.loadtxt(INFILE, dtype=int, delimiter='\n')
+
+    prev = in_data[0]
+    counter = 0
+    for val in in_data[1:]:
+        if val > prev:
+            counter += 1
+        prev = val
+    return counter
 
 
 if __name__ == '__main__':
-    main()
+    print("The number of increases in seafloor elevation is: %d." % main())
