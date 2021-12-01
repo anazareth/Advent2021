5164301e5a8829ec06d39c2e6dd23c9c96770a12
anazareth <anazareth@ryerson.ca>
2021-12-01 16:54:57 -0500

    part 1 redundant, part 2 solution applies to part 1 problem

--- day1/day1.py
+++ day1/day1.py
@@ -7,18 +7,6 @@ import numpy as np
 INFILE = r'day1/day1_input'
 
 
-def part1():
-    in_data = np.loadtxt(INFILE, dtype=int, delimiter='\n')
-
-    prev = in_data[0]
-    counter = 0
-    for val in in_data[1:]:
-        if val > prev:
-            counter += 1
-        prev = val
-    return counter
-
-
 def part2(w_size):
     in_data = np.loadtxt(INFILE, dtype=int, delimiter='\n')
 
@@ -35,6 +23,6 @@ def part2(w_size):
 
 
 if __name__ == '__main__':
-    print("The number of increases in seafloor elevation is: %d." % part1())
+    print("The number of increases in seafloor elevation is: %d." % part2(1))
     print("When considering the sum of the last 3 data points, "
           "the number of increases in seafloor elevation is: %d." % part2(3))
