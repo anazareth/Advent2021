48d03b6ffa3939857dc01e5a7a7788fcdc58d8e2
anazareth <anazareth@ryerson.ca>
2021-12-01 16:53:49 -0500

    day 1 part 2 done

--- day1/day1.py
+++ day1/day1.py
@@ -7,7 +7,7 @@ import numpy as np
 INFILE = r'day1/day1_input'
 
 
-def main():
+def part1():
     in_data = np.loadtxt(INFILE, dtype=int, delimiter='\n')
 
     prev = in_data[0]
@@ -19,5 +19,22 @@ def main():
     return counter
 
 
+def part2(w_size):
+    in_data = np.loadtxt(INFILE, dtype=int, delimiter='\n')
+
+    prev = sum(in_data[0:w_size])
+    counter = 0
+    i = w_size
+    for val in in_data[w_size:]:
+        rollsum = sum(in_data[i-w_size+1:i+1])
+        if rollsum > prev:
+            counter += 1
+        prev = rollsum
+        i += 1
+    return counter
+
+
 if __name__ == '__main__':
-    print("The number of increases in seafloor elevation is: %d." % main())
+    print("The number of increases in seafloor elevation is: %d." % part1())
+    print("When considering the sum of the last 3 data points, "
+          "the number of increases in seafloor elevation is: %d." % part2(3))
