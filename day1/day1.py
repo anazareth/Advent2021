3ca671a24ba29643c127e35ae9e4672d7710f410
anazareth <anazareth@ryerson.ca>
2021-12-03 12:53:52 -0500

    add problem description and docstring

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,18 @@
-# Problem: https://adventofcode.com/2021/day/1
+# Problem: Sonar Sweep
+# URL: https://adventofcode.com/2021/day/1
 # Date: December 1st, 2021
 # Author: Alex Nazareth
+
+'''
+Input text file with one integer on each line, n-many lines.
+Output number of integers which increase from the previous integer.
+Eg. Max output n-1, in case of all numbers increasing.
+For second part, compare rolling sums for increase, with a window of 3.
+i.e. Is sum of 2nd, 3rd, 4th integers greater than the sum of 1st + 2nd + 3rd?
+    And so on, until the (n-2)th, (n-1)th, and nth.
+Note first part same as second part, only with window size 1 instead of 3.
+'''
+
 
 import numpy as np
 
--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,15 @@
-# Problem: https://adventofcode.com/2021/day/2
+# Problem: Dive!
+# URL: https://adventofcode.com/2021/day/2
 # Date: December 2nd, 2021
 # Author: Alex Nazareth
+
+'''
+Input text file where each row contains a command {forward, up, down} and
+    a positive integer value. E.g. "forward 5" or "up 3".
+Commands have different effects in part 1 and part 1, see respective functions for details.
+Output is final horizontal position of submarine multpilied with final depth,
+with both values initialized at 0.
+'''
 
 import numpy as np
 
@@ -8,6 +17,12 @@ INFILE = r'day2_input'
 
 
 def part1(data):
+    '''
+    Commands "up" and "down" decrease and increase depth, respectively.
+    Command "forward" increases horizontal position.
+    :param data: list of strings
+    :return: integer
+    '''
     h_pos, d_pos = 0, 0  # horizontal position, depth
     for i in data:
         direction, val = i.split(' ')[0], int(i.split(' ')[1])
@@ -23,6 +38,13 @@ def part1(data):
 
 
 def part2(data):
+    '''
+    Commands "up" and "down" decrease and increase aim, respectively.
+    Command "forward" increases horizontal position AND increases depth by aim
+        multiplied by value.
+    :param data: list of strings
+    :return: integer
+    '''
     h_pos, d_pos, aim = 0, 0, 0  # horizontal position, depth, aim
     for i in data:
         direction, val = i.split(' ')[0], int(i.split(' ')[1])
