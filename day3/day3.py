43160839344316e651e2641d99a439f3353e92fc
anazareth <anazareth@ryerson.ca>
2021-12-03 14:43:59 -0500

    add docstrings to functions

--- day3/day3.py
+++ day3/day3.py
@@ -5,11 +5,8 @@
 
 '''
 Input text file with binary number on each row.
-Output "gamma rate" multiplied with "epsilon rate", both binary numbers where:
--"gamma rate" defines each bit as most common bit in that position in the file.
--"epsilon rate" is the same, but with LEAST common bit in each position.
-- e.g. for 001, 011, 001, we have gamma rate 001 and epsilon rate 110
-- output of this example is 1*8 = 8.
+Output product of two binary numbers calculated.
+See part 1 and part 2 for different subproblems.
 '''
 
 import numpy as np
@@ -17,6 +14,17 @@ INFILE = r'day3_input'
 
 
 def part1(data):
+    '''
+    Input list of binary numbers.
+    Output product of "gamma rate" and "epsilon rate" where:
+    -"gamma rate" defines each bit as most common bit in that position in the file.
+    -"epsilon rate" is the same, but with LEAST common bit in each position.
+    - e.g. for 001, 011, 001, we have gamma rate 001 and epsilon rate 110
+-    output of this example is 1*8 = 8.
+    :param data: list of strings
+    :return: integer
+    '''
+
     gamma_rate, epsilon_rate = 0, 0
     for bit_pos in range(len(data[0])):
         count_bits = {'0': 0, '1': 0}
@@ -30,12 +38,31 @@ def part1(data):
 
 
 def part2(data) -> int:
+    '''
+    Input list of binary numbers.
+    Output product of "oxygen generator rating" and "CO2 scrubber rating" where:
+    - both values defined by going through each bit position and discarding subset of numbers
+    - for O2 rating, keep only numbers with most frequent bit in that position,
+        keep 1's in case of tie
+    - for CO2 rating, keep LESS frequent, keep 0's in case of tie
+    :param data: list of strings
+    :return: integer
+    '''
+
     o2 = reduce(data, 0, keep=True)
     co2 = reduce(data, 0, keep=False)
     return int(o2, 2)*int(co2, 2)
 
 
 def reduce(numbers, pos, keep):
+    '''
+    Recursive function to reduce list of values based on most common bit in
+    position "pos".
+    :param numbers: list of strings representing binary numbers
+    :param pos: integer representing current bit position
+    :param keep: boolean indicating if we keep the most common or discard
+    :return: string representing final binary number after all reduction
+    '''
     if len(numbers) == 1:
         return numbers[0]
     one_nums, zero_nums = [], []
