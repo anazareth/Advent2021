2e1c790a7bdedda9dbc0ff9a4ce6128eb4610da3
anazareth <anazareth@ryerson.ca>
2021-12-10 12:46:07 -0500

    clean up day 6

--- day6/day6.py
+++ day6/day6.py
@@ -7,14 +7,13 @@
 Modeling lanternfish population.
 - each fish spawns a new one after 7 days (+ 2 days to grow after initial spawn)
 - define each fish by number of days until it spawns a new one
--
 """
 
 import numpy as np
 INFILE = r'day6_input'  # text file with input data provided by AoC2021
 
 
-def part1(data, n):
+def main(data, n):
     """
 
     :param n: int, number of days to simulate
@@ -25,17 +24,7 @@ def part1(data, n):
     new_fish_value = 8
     just_spawned_value = 6
 
-    # # OLD and inefficient implementation
-    # for day in range(n):
-    #     new_spawns = []
-    #     data += -1  # decrease fish counters
-    #     for i, fish in enumerate(data):
-    #         if fish == -1:  # fish just spawned
-    #             new_spawns.append(new_fish_value)  # add a new one
-    #             data[i] = just_spawned_value  # reset counter
-    #     data = np.append(data, new_spawns)
-
-    # smart implementation: track count of fish in each group by time-to-spawn
+    # track count of fish in each group by time-to-spawn
     count_fish = dict.fromkeys(range(new_fish_value + 1), 0)
     for f in data:
         count_fish[f] += 1
@@ -52,4 +41,4 @@ if __name__ == '__main__':
     DAYS_TO_SIMULATE = 256
     raw_data = np.loadtxt(INFILE, delimiter=',', dtype=int)
     print(f'The number of lanternfish after {DAYS_TO_SIMULATE} days is'
-          f' {part1(raw_data, DAYS_TO_SIMULATE)}')
+          f' {main(raw_data, DAYS_TO_SIMULATE)}')
